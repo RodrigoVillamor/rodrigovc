d15154fcb35aee2debdd942e7e0c0be77f016767
Rodrigo <rodrigovillamorcas16@gmail.com>
2026-05-01 13:04:17 +0200

    Resolución de conflicto

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        saludar();
+    }
+
+    private static void saludar() {
+        System.out.println("Hola mundo conflicto");
+    }
+}
